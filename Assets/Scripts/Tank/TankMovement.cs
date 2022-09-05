4c3dce6e2d76710fc7e8f92b47d69366ca110d7b
kerotan223 <kerotan223@gmail.com>
2022-09-05 23:02:49 +0900

    戦車移動作成

---
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 戦車移動
+/// ↑↓で前進後退
+/// ←→で自身のY軸を中心に回転をする
+/// 移動方法にRigidbodyを使用する
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class TankMovement : MonoBehaviour
+{
+    [SerializeField,Tooltip("移動速度")]
+    private float m_Speed = 1f;
+    [SerializeField, Tooltip("回転速度")]
+    private float m_TurnSpeed = 180f;
+
+    // 1フレームの速度
+    public Vector3 Velocity { get; protected set; }
+
+    // 移動用リジッドボディ
+    private Rigidbody m_Rigidbody;
+    private Vector3 m_PreviousPosition; // 前座標
+    private Vector3 m_CurrentPosition;  // 現在座標
+    private Vector3 m_NextMovement;     // 移動量
+
+    private Vector3 m_PreviousRotation; // 前回転
+    private Vector3 m_CurrentRotation;  // 現在回転
+    private Quaternion m_NextRotate;       // 回転量
+
+
+    private void Awake()
+    {
+        // アタッチ済みコンポーネント取得
+        m_Rigidbody = GetComponent<Rigidbody>();
+        if (m_Rigidbody != null)
+            m_CurrentPosition = m_PreviousPosition = m_Rigidbody.position;
+    }
+
+    private void Update()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        Turn(hor);
+        Move(ver);
+    }
+
+    private void FixedUpdate()
+    {
+        // 座標と回転更新
+        PositionUpdate();
+        RotationUpdate();
+    }
+
+    /// <summary>
+    /// 座標更新
+    /// </summary>
+    private void PositionUpdate()
+    {
+        // 移動量から次の座標を設定する
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_NextMovement);
+        m_NextMovement = Vector3.zero;
+    }
+
+    /// <summary>
+    /// 回転更新
+    /// </summary>
+    private void RotationUpdate()
+    {
+        Quaternion q = (m_Rigidbody.rotation * m_NextRotate).normalized;
+        m_Rigidbody.MoveRotation(q);
+        m_NextRotate = Quaternion.identity;
+    }
+
+    /// <summary>
+    /// シンプルに座標移動
+    /// </summary>
+    /// <param name="speedScale">移動倍率</param>
+    public void Move( float speedScale = 1.0f)
+    {
+        m_NextMovement = transform.forward * speedScale * m_Speed * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Y軸を中心に座標回転
+    /// </summary>
+    /// <param name="speedScale">回転倍率　兼　回転方向</param>
+    public void Turn(float speedScale = 1.0f)
+    {
+        float turn = speedScale * m_TurnSpeed * Time.deltaTime;
+        m_NextRotate = Quaternion.Euler(0, turn, 0).normalized;
+    }
+}
