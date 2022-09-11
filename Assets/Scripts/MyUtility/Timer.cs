9e2d15cb69624fcdd41c70ac1787f595653d127d
kerotan223 <kerotan223@gmail.com>
2022-09-11 14:35:52 +0900

    経過時間測定クラスを作成 砲弾が衝突判定に当たらない場合のセーフティを追加

---
+++ Assets/Scripts/MyUtility/Timer.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 時間計測クラス
+/// </summary>
+public class Timer
+{
+    // 経過時間
+    private float m_CurrentTime = 0.0f;
+
+    // 目標時間
+    private float m_EndTime = 0.0f;
+
+    // タイマー起動フラグ(trueで動く)
+    private bool m_IsAwake = false;
+
+    // タイマー完了時実行処理
+    public Action OnComplete = null;
+
+    /// <summary>
+    /// タイマー起動
+    /// </summary>
+    /// <param name="end">目標時間設定</param>
+    /// <param name="offset">経過時間オフセット(基本は0秒経過指定)</param>
+    public void Awake(float end,float offset = 0.0f)
+    {
+        m_CurrentTime = offset;
+        m_EndTime = end;
+        m_IsAwake = true;
+    }
+
+    /// <summary>
+    /// Time.deltaTimeにより経過時間を更新
+    /// </summary>
+    /// <returns>指定した経過時間を超えた場合はtrueを返す</returns>
+    public bool UpdateWithDeltaTime()
+    {
+        bool res = false;
+
+        // そもそも起動していないなら更新しない
+        if (!m_IsAwake) return res;
+
+        // deltaTimeで経過時間を計測
+        m_CurrentTime += Time.deltaTime;
+        if(m_CurrentTime > m_EndTime)
+        {
+            res = true;
+            // 終了時の関数を実行
+            OnComplete?.Invoke();
+            // 一度計測を終了したらリセット
+            // ※ループしたい場合は別途処理を修正する
+            Reset();
+        }
+
+        return res;
+    }
+
+    /// <summary>
+    /// タイマーリセット
+    /// </summary>
+    public void Reset()
+    {
+        m_IsAwake = false;
+        m_CurrentTime = 0.0f;
+    }
+}
--- Assets/Scripts/Shell/Shell.cs
+++ Assets/Scripts/Shell/Shell.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 砲弾クラス
+/// 射出した砲弾の制御
+/// </summary>
 [RequireComponent(typeof(Rigidbody))]
 public class Shell : MonoBehaviour
 {
@@ -9,9 +13,26 @@ public class Shell : MonoBehaviour
     public ShellObject m_ShellObject;
     public Rigidbody m_Rigidbody { get; private set; }
 
+    // 解放までの時間
+    // 長すぎず短すぎずの塩梅を指定してください
+    [SerializeField]
+    private float releaseTime = 5.0f;
+
+    // 解放タイマー
+    // 衝突判定を検知せず自己解放ができない可能性を考慮
+    // 指定時間を超えても解放されない場合はタイマーで解放させる
+    // 基本的に使用しないが、安全マージンとして実装する
+    private Timer releaseTimer = new Timer();
+
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        releaseTimer.OnComplete = Release;
+    }
+
+    private void Update()
+    {
+        releaseTimer.UpdateWithDeltaTime();
     }
 
     /// <summary>
@@ -21,17 +42,22 @@ public class Shell : MonoBehaviour
     {
         transform.position = pos;
         transform.rotation = rot;
+        releaseTimer.Awake(releaseTime);
     }
 
+    /// <summary>
+    /// 解放処理
+    /// </summary>
     private void Release()
     {
+        // プールに戻す
         m_ShellObject.ReturnToPool();
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTriggerEnter : " + other.name);
+        MyDebug.Log("OnTriggerEnter: " + other.name);
         Release();
     }
 }
