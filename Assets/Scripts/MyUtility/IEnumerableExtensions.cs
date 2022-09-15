3bed380198dd826baf06717951d32bfdb6c6ec97
kerotan223 <kerotan223@gmail.com>
2022-09-15 21:38:46 +0900

    セレクターで配列ソートする処理を追加

--- Assets/Scripts/MyUtility/IEnumerableExtensions.cs
+++ Assets/Scripts/MyUtility/IEnumerableExtensions.cs
@@ -19,4 +19,17 @@ public static class IEnumerableExtensions
     {
         return self.First(c => selector(c).Equals(self.Max(selector)));
     }
+
+    /// <summary>
+    /// 配列内の要素を指定キーでソートします
+    /// </summary>
+    public static void Sort<TSource, TResult>(this TSource[] array,Func<TSource, TResult> selector) where TResult : IComparable
+    {
+        Array.Sort(array, (x, y) =>
+        {
+            var result = selector(x).CompareTo(selector(y));
+            return result;
+        });
+    }
+
 }
