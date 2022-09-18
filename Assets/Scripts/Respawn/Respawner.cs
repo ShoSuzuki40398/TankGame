a9fff223017915456880fb52af6bc4d79a6374af
kerotan223 <kerotan223@gmail.com>
2022-09-18 16:40:49 +0900

    戦車のリスポーン処理を追加

---
+++ Assets/Scripts/Respawn/Respawner.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シーン開始時に子要素に存在する「RespawnPoint」タグが付いたオブジェクトを
+/// リスポーン地点として保持する
+/// リスポーン要求に応じて保持しているオブジェクトの情報を返す
+/// </summary>
+public class Respawner : MonoBehaviour
+{
+    // リスポーン位置リスト
+    private GameObject[] m_RespawnPoints = null;
+
+    // リスポーンタグ
+    private const string m_RespawnTag = "RespawnPoint";
+
+    // リスポーン種類
+    public enum RespawnMode
+    {
+        RANDOM, // ランダムリスポーン
+        FAR,    // 最遠距離リスポーン
+        NEAR    // 最近距離リスポーン
+    }
+    [SerializeField]
+    private RespawnMode m_RespawnMode = RespawnMode.RANDOM;
+
+
+    private void Awake()
+    {
+        m_RespawnPoints = GameObject.FindGameObjectsWithTag(m_RespawnTag);
+    }
+
+    /// <summary>
+    /// リスポーン位置を取得
+    /// </summary>
+    /// <returns></returns>
+    public Transform GetRespawnPoint(Vector3 target)
+    {
+        Transform res = transform;
+
+        switch (m_RespawnMode)
+        {
+            case RespawnMode.RANDOM: res = GetRandomRespawnPoint(); break;
+            case RespawnMode.FAR: res = GetFarRespawnPoint(target); break;
+            case RespawnMode.NEAR: res = GetNearRespawnPoint(target); break;
+        }
+
+        return res;
+    }
+
+    /// <summary>
+    /// リスポーン位置が存在するか
+    /// リスポーン位置がないというパターンは基本的に想定していないので
+    /// あらかじめ1つ以上はリスポーン用のオブジェクトを保持しておくこと。
+    /// </summary>
+    /// <returns></returns>
+    private bool ExistRespawnPoint()
+    {
+        if (m_RespawnPoints == null) return false;
+        if (m_RespawnPoints.Length == 0) return false;
+
+        return true;
+    }
+
+    /// <summary>
+    /// ランダムなリスポーン位置を取得する
+    /// </summary>
+    /// <returns></returns>
+    private Transform GetRandomRespawnPoint()
+    {
+        Transform res = transform;
+
+        // リスポーン位置がない場合は自分の位置を返す。
+        if (!ExistRespawnPoint())
+            return res;
+
+        // ランダムに配列のオブジェクトのインデックスを決定して返す
+        int index = Random.Range(0, m_RespawnPoints.Length);
+        
+        return m_RespawnPoints[index].transform;
+    }
+
+    /// <summary>
+    /// 指定した位置から一番遠いリスポーン位置を取得する
+    /// </summary>
+    /// <returns></returns>
+    private Transform GetFarRespawnPoint(Vector3 target)
+    {
+        Transform res = transform;
+
+        // リスポーン位置がない場合は自分の位置を返す。
+        if (!ExistRespawnPoint())
+            return res;
+
+        // 最大距離を測る
+        // 最初に配列先頭のリスポーン位置への距離を最大距離として保持する。
+        // その後先頭以外のリスポーン位置と比較して最大距離のリスポーン位置を返す
+        // ※変更案：距離の閾値を決めて、一定の距離以上離れたリスポーン位置を返す
+        // ※全てのリスポーン位置と比較する必要がないためスループットは上がる
+        Vector3 respawnPos = m_RespawnPoints[0].transform.position;
+        float maxDistance = Vector3.Distance(m_RespawnPoints[0].transform.position,target);
+        int index = 0;
+        Debug.Log("距離：" + maxDistance + " , " + "番号：" + index);
+        for (int i = index + 1; i < m_RespawnPoints.Length; ++i)
+        {
+            respawnPos = m_RespawnPoints[i].transform.position;
+            float distance = Vector3.Distance(respawnPos, target);
+
+            Debug.Log("距離：" + distance + " , " + "番号：" + index);
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+                index = i;
+            }
+        }
+
+        Debug.Log("決定　：" + "距離：" + maxDistance + " , " + "番号：" + index);
+        return m_RespawnPoints[index].transform;
+    }
+
+    /// <summary>
+    /// 指定した位置から一番遠いリスポーン位置を取得する
+    /// </summary>
+    /// <returns></returns>
+    private Transform GetNearRespawnPoint(Vector3 target)
+    {
+        Transform res = transform;
+
+        // リスポーン位置がない場合は自分の位置を返す。
+        if (!ExistRespawnPoint())
+            return res;
+
+        // 最小距離を測る
+        // 最初に配列先頭のリスポーン位置への距離を最小距離として保持する。
+        // その後先頭以外のリスポーン位置と比較して最小距離のリスポーン位置を返す
+        Vector3 respawnPos = m_RespawnPoints[0].transform.position;
+        float minDistance = Vector3.Distance(m_RespawnPoints[0].transform.position, target);
+        int index = 0;
+
+        for (int i = index + 1; i < m_RespawnPoints.Length; ++i)
+        {
+            float distance = Vector3.Distance(respawnPos, target);
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                index = i;
+            }
+        }
+
+        return m_RespawnPoints[index].transform;
+    }
+}
---
+++ Assets/Scripts/Tank/TankRespawn.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 戦車リスポーン制御
+/// 事前にシーンに配置した「Respowner」に対してリスポーン座標要求を行います
+/// リスポーン時の処理はこちらのクラスで担当します。（HPバーのリセットや戦車の初期化等）
+/// </summary>
+public class TankRespawn : MonoBehaviour
+{
+    // リスポーン制御
+    [SerializeField]
+    private Respawner m_Respawner;
+
+    // HP表示
+    [SerializeField]
+    private TankHealth m_TankHealth;
+
+    // リスポーン開始までの時間
+    [SerializeField]
+    private float m_StartRespawnCount = 2.5f;
+    
+    /// <summary>
+    /// リスポーン開始
+    /// </summary>
+    public void Respawn(Damager damager,Damageable damageable)
+    {
+        this.Delay(m_StartRespawnCount, ()=>Initialize(damager,damageable));
+    }
+    
+    /// <summary>
+    /// 座標、パラメーター等の初期化
+    /// </summary>
+    /// <param name="damager"></param>
+    /// <param name="damageable"></param>
+    private void Initialize(Damager damager, Damageable damageable)
+    {
+        // 死んだ場所をリスポーン制御に送り、リスポーン位置を取得する
+        Vector3 pos = transform.position;
+        Transform respawnPoint = m_Respawner.GetRespawnPoint(pos);
+
+        // 位置と回転の初期化
+        transform.position = respawnPoint.position;
+        transform.rotation = respawnPoint.rotation;
+
+        damageable.ResetHealth();
+        m_TankHealth.HPChange(damageable.CurrentHealth);
+
+    }
+}
